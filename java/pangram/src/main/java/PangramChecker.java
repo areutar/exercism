19a86f656bec3bc7c7a1d8c493ee42d8179e59b9
areutar <areuta@rambler.ru>
2023-10-27 05:47:50 +0300

    simplified PangramChecker

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,10 +1,9 @@
 public class PangramChecker {
     public boolean isPangram(String input) {
-        return input.toLowerCase()
-                .replaceAll("\\P{Alpha}", "")
-                .chars()
+        String alphabet = "abcdefghijklmnopqastuvwxyz";
+        return alphabet.chars()
                 .mapToObj(i -> (char) i)
-                .distinct()
+                .filter(c -> input.toLowerCase().indexOf(c) >= 0)
                 .count() == 26;
     }
 }
